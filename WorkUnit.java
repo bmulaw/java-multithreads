ecd1dfa357078f7525e025c2de982770f3997677
bmulaw <67084560+bmulaw@users.noreply.github.com>
2021-11-07 15:01:05 -0500

    professor's code

---
+++ WorkUnit.java
@@ -0,0 +1,57 @@
+package hw6;
+
+/***************************************************/
+/* CS-350 Fall 2021 - Homework 6 - Code Solution   */
+/* Author: Renato Mancuso (BU)                     */
+/*                                                 */
+/* Description: This class implements a descriptor */
+/*   for a unit of work that needs to be processed */
+/*   by the generic worker thread. The class also  */
+/*   incorporates a field to save the result of    */
+/*   the performed computation.                    */
+/*                                                 */
+/***************************************************/
+
+public class WorkUnit {
+
+    String hash;
+    String result;
+
+    /* Simple constructor to set the input hash */
+    public WorkUnit (String hash) {
+	this.hash = hash;
+	this.result = null;
+    }
+
+    public String getHash() {
+	return hash;
+    }
+
+    /* These can be handy to generalize the boundaries of hash
+     * cracking */
+    public int getLowerBound() {
+	return 0;
+    }
+
+    public int getUpperBound() {
+	return Integer.MAX_VALUE;
+    }
+
+    public void setResult(String result) {
+	this.result = result;
+    }
+
+    public String getResult() {
+	return result;
+    }
+
+    /* Render this WorkUnit when printed */
+    @Override
+    public String toString() {
+	if (this.result != null)
+	    return this.result;
+	else
+	    return this.hash;
+    }
+    
+}
